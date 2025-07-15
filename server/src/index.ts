4c6e38611df56e6c4593b599dd5aaeff47647a1d
mushfiqbh <mushfiqbh@gmail.com>
2025-07-16 00:10:28 +0600

    Bump Version to 2.11

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.status(200).json({
     live: true,
-    version: "2.10.0",
+    version: "2.11.0",
     message: "Server is running",
   });
 });
