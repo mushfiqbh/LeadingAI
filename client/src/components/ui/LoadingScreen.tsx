30a5b209b8597d7fa77fa59c564e9b7ace62302d
mushfiqbh <mushfiqbh@gmail.com>
2026-05-13 21:05:52 +0600

    Clean Repo for Rewiring Frontend and RAG Backend

--- client/src/components/ui/LoadingScreen.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function LoadingScreen() {
-  return (
-    <div className="w-full min-h-screen flex items-center justify-center text-gray-500">
-      <p>Loading...</p>
-    </div>
-  );
-}
